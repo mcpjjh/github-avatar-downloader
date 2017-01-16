291c4debc8c6d7f897eb2df3b78d891d1c7a1c63
Young <mcpjjh@gmail.com>
2017-01-16 22:43:18 +0000

    defined function getRepoContributors and created requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
+  // console.log(requestURL);
+
+var GITHUB_USER = "mcpjjh";
+var GITHUB_TOKEN = "41530ef33cab32c42a7918d1bc87b8e09d98abd9";
+
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+console.log(requestURL);
+
+}
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+  // console.log(requestURL);
+  })
