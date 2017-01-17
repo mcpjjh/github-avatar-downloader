bb5db7e1aafd15f597ca7e1c54973edcbc2e2710
Young <mcpjjh@gmail.com>
2017-01-17 02:17:28 +0000

    added process.argv

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,7 @@ var request = require('request');
 var fs = require('fs');
 var fetch = require('isomorphic-fetch')
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
-  // console.log(requestURL);
+var args = process.argv.slice(2)
 
 var GITHUB_USER = "mcpjjh";
 var GITHUB_TOKEN = "41530ef33cab32c42a7918d1bc87b8e09d98abd9";
@@ -41,7 +40,7 @@ function downloadImageByURL(url, login) {
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(args[0], args[1], function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
   })
